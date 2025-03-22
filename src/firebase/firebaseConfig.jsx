5393729a18b7106380fb59bdb749ede45df790c3
Sivanesh P <154664183+sivanh1@users.noreply.github.com>
2025-03-22 15:03:42 +0530

    Update firebaseConfig.jsx

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -7,12 +7,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZNX3TQUIenlbUGn8xQZwDDsnvZcw37V8",
-  authDomain: "final-toll.firebaseapp.com",
-  projectId: "final-toll",
-  storageBucket: "final-toll.firebasestorage.app",
-  messagingSenderId: "469299343647",
-  appId: "1:469299343647:web:74a9d4b187853464ae402a"
 };
 
 const app = initializeApp(firebaseConfig);
